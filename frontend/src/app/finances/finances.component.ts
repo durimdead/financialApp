35a9f172759d3679cb5e38d84f5d5ce37fcc2a5f
durimdead <david.lancellotti@gmail.com>
2025-02-26 12:17:29 -0500

    fixed routerlink import

--- frontend/src/app/finances/finances.component.ts
+++ frontend/src/app/finances/finances.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-finances',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './finances.component.html',
   styleUrl: './finances.component.css'
 })

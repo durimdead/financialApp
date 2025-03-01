74e740606484e6006756aff21554b49c33b365ec
durimdead <david.lancellotti@gmail.com>
2025-03-01 11:25:58 -0500

    basic form, single input showing in "add" modal

--- frontend/src/app/material-test/dialog-add-element/dialog-add-element.component.ts
+++ frontend/src/app/material-test/dialog-add-element/dialog-add-element.component.ts
@@ -1,16 +1,40 @@
-import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogModule} from '@angular/material/dialog';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {FormControl, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {merge} from 'rxjs';
 import { PeriodicElement } from '../../../app.interfaces';
+import { MatDialogModule} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dialog-add-element',
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatDialogModule, MatButtonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './dialog-add-element.component.html',
   styleUrl: './dialog-add-element.component.css'
 })
 export class DialogAddElementComponent {
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+
+  errorMessage = signal('');
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
 
   submitNewElement(){
     let newElement: PeriodicElement = {

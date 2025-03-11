59c7d09b51885c25a43e69bb3f5670ccfaf9bfb6
durimdead <david.lancellotti@gmail.com>
2025-03-11 14:16:59 -0400

    added new conteoller for PeriodicElements and took out the WeatherForecast API

---
+++ api/FinanceApi/FinanceApi/Controllers/PeriodicElementsController.cs
@@ -0,0 +1,60 @@
+﻿using FinanceApi.Models;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using FinanceApi.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FinanceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeriodicElementsController : ControllerBase
+    {
+        private readonly ILogger<PeriodicElementsController> _logger;
+        private readonly ElementService _elementService;
+
+        public PeriodicElementsController(ILogger<PeriodicElementsController> logger)
+        {
+            _logger = logger;
+            _elementService = new ElementService();
+        }
+        // GET: api/<PeriodicElements>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var elements = this._elementService.GetElements();
+            var jsonData = new { httpStatusCode = HttpStatusCode.OK, elementData = elements, errorMessage = "" };
+            if (elements == null)
+            {
+                jsonData = new { httpStatusCode = HttpStatusCode.InternalServerError, elementData = new PeriodicElement[] { }, errorMessage = "There was an error retrieving element data" };
+            }
+            return new JsonResult(jsonData);
+        }
+
+        // GET api/<PeriodicElements>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PeriodicElements>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PeriodicElements>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PeriodicElements>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

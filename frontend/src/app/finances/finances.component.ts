eacc726e50bce880ddb442a0946bc5e2a679ddec
durimdead <david.lancellotti@gmail.com>
2025-03-24 14:18:24 -0400

    started working on the tab for finances.

--- frontend/src/app/finances/finances.component.ts
+++ frontend/src/app/finances/finances.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-// import {MatTableModule, MatTableDataSource} from '@angular/material/table'
+import {MatTableModule} from '@angular/material/table'
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-finances',
-  imports: [RouterLink],
+  imports: [MatTableModule, MatSortModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './finances.component.html',
-  styleUrl: './finances.component.css'
+  styleUrl: './finances.component.css',
 })
 export class FinancesComponent {
   //TODO: start filling in with code for the finance component items.
+
+  openAddExpenseModal(): void {
+    console.log('adding expense');
+  }
 }

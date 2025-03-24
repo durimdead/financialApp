c80e902a6641173dab01c58937b65f30498aa779
durimdead <david.lancellotti@gmail.com>
2025-03-24 14:53:14 -0400

    - set up the table structure for finances - set up the crud entry-point functions for finances

--- frontend/src/app.interfaces.ts
+++ frontend/src/app.interfaces.ts
@@ -16,3 +16,19 @@ export interface ElementApiGet {
   elementData: PeriodicElement[];
   errorMessage: string;
 }
+
+export interface ExpenseData {
+	//TODO: see if we can remove this item
+	actions: string;
+	expenseId: number;
+	expenseDate: Date;
+	expenseDescription: string;
+	expenseAmount: number;
+	//TODO: update to be an actual enum-ish type
+	expenseType: string;
+	//TODO: update to be an actual enum-ish type
+	expensePaymentType: string;
+	//TODO: also an enum-ish type??
+	expensePaymentDescription: string;
+}
+
--- frontend/src/app/finances/finances.component.ts
+++ frontend/src/app/finances/finances.component.ts
@@ -1,20 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {MatTableModule} from '@angular/material/table'
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSortModule } from '@angular/material/sort';
+import { ExpenseData } from '../../app.interfaces';
 
 @Component({
   selector: 'app-finances',
-  imports: [MatTableModule, MatSortModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [
+    MatTableModule,
+    MatSortModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+  ],
   templateUrl: './finances.component.html',
   styleUrl: './finances.component.css',
 })
 export class FinancesComponent {
   //TODO: start filling in with code for the finance component items.
+  private expenses = signal<ExpenseData[]>([]);
+  dataSource = new MatTableDataSource(this.expenses());
+  displayedColumns: string[] = [
+    'actions',
+    'expenseDate',
+    'expenseDescription',
+    'expenseAmount',
+	'expenseType',
+	'expensePaymentType',
+	'expensePaymentDescription',
+  ];
 
   openAddExpenseModal(): void {
     console.log('adding expense');
+  }
+
+  editExpense(expenseId: number): void {
+    console.log('editing expense');
+  }
+
+  confirmDeleteExpense(expenseId: number): void {
+    console.log('deleting expense');
   }
 }

da833743d173fff17603defb360d546d1d053e34
durimdead <david.lancellotti@gmail.com>
2025-03-30 14:46:55 -0400

    added class for vPaymentTypeCategory view

---
+++ api/FinanceApi/FinanceApi/Repositories/EF Models/vPaymentTypeCategory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApi.Repositories.EF_Models
+{
+    public class vPaymentTypeCategory
+    {
+        [Key]
+        public int PaymentTypeCategoryID { get; set; }
+        public required string PaymentTypeCategoryName { get; set; }
+
+    }
+}

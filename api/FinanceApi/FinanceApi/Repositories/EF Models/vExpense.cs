8c4b29c0405f5e2caeba22ff290a1e1710273ca1
durimdead <david.lancellotti@gmail.com>
2025-03-30 14:35:18 -0400

    Created model for vExpenseDetails

---
+++ api/FinanceApi/FinanceApi/Repositories/EF Models/vExpense.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApi.Repositories.EF_Models
+{
+    public class vExpenseDetails
+    {
+        [Key]
+        public int ExpenseID { get; set; }
+        public string ExpenseTypeName { get; set; }
+        public string PaymentTypeName { get; set; }
+        public string PaymentTypeCategoryName { get; set; }
+        public bool IsIncome { get; set; }
+        public bool IsInvestment { get; set; }
+        public int ExpenseTypeID {  get; set; }
+        public int PaymentTypeID { get; set; }
+        public string PaymentTypeDescription { get; set; }
+        public int PaymentTypeCategoryID { get; set; }
+    }
+}

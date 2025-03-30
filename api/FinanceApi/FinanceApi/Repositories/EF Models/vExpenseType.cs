1e214680600d59b5c6767b08a07eb3ecf563f35f
durimdead <david.lancellotti@gmail.com>
2025-03-30 14:46:01 -0400

    Created class for vExpenseType view

---
+++ api/FinanceApi/FinanceApi/Repositories/EF Models/vExpenseType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApi.Repositories.EF_Models
+{
+    public class vExpenseType
+    {
+        [Key]
+        public int ExpenseTypeID { get; set; }
+        public required string ExpenseTypeName { get; set; }
+        public required string ExpenseTypeDescription { get; set; }
+    }
+}

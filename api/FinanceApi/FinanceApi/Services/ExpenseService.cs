8a253567a73632370d1bcabea95b3c424cfd2273
durimdead <david.lancellotti@gmail.com>
2025-03-31 14:08:18 -0400

    Added in the content for the rest of the "Add" methods for the database tables

--- api/FinanceApi/FinanceApi/Services/ExpenseService.cs
+++ api/FinanceApi/FinanceApi/Services/ExpenseService.cs
@@ -19,7 +19,6 @@ namespace FinanceApi.Services
             string argumentOutOfRangeMessage = string.Empty;
             if (expenseTypeID <= 0)
             {
-                argumentOutOfRangeMessage += argumentOutOfRangeMessage != string.Empty ? "\r\n" : string.Empty;
                 argumentOutOfRangeMessage += "expenseTypeID must be a positive integer. Current value : " + expenseTypeID;
             }
             if (paymentTypeID <= 0)
@@ -57,17 +56,71 @@ namespace FinanceApi.Services
 
         public void AddExpenseType(string expenseTypeName, string expenseTypeDescription)
         {
-            throw new NotImplementedException();
+            // ensure all strings are trimmed
+            expenseTypeName = expenseTypeName.Trim();
+            expenseTypeDescription = expenseTypeDescription.Trim();
+
+            try
+            {
+                // attempt to upsert the Expense Type
+                this._context.usp_ExpenseTypeUpsert(expenseTypeName, expenseTypeDescription);
+            }
+            catch (Exception ex)
+            {
+                // log the error and then re-throw it to ensure anywhere else that needs to handle the error can still do so
+                this._logger.LogError(ex.InnerException.Message);
+                throw;
+            }
         }
 
         public void AddPaymentType(string paymentTypeName, string paymentTypeDescription, int paymentTypeCategoryID)
         {
-            throw new NotImplementedException();
+            // check for invalid IDs for the insert
+            string argumentOutOfRangeMessage = string.Empty;
+            if (paymentTypeCategoryID <= 0)
+            {
+                argumentOutOfRangeMessage += "paymentTypeCategoryID must be a positive integer. Current value : " + paymentTypeCategoryID;
+            }
+
+            // if any were found, throw an error
+            if (argumentOutOfRangeMessage != string.Empty)
+            {
+                throw new ArgumentOutOfRangeException(argumentOutOfRangeMessage);
+            }
+
+            // ensure all strings are trimmed
+            paymentTypeName = paymentTypeName.Trim();
+            paymentTypeDescription = paymentTypeDescription.Trim();
+
+            try
+            {
+                // attempt to upsert the Expense
+                this._context.usp_PaymentTypeUpsert(paymentTypeName, paymentTypeDescription, paymentTypeCategoryID);
+            }
+            catch (Exception ex)
+            {
+                // log the error and then re-throw it to ensure anywhere else that needs to handle the error can still do so
+                this._logger.LogError(ex.InnerException.Message);
+                throw;
+            }
         }
 
         public void AddPaymentTypeCategory(string paymentTypeCategoryName)
         {
-            throw new NotImplementedException();
+            // ensure all strings are trimmed
+            paymentTypeCategoryName = paymentTypeCategoryName.Trim();
+
+            try
+            {
+                // attempt to upsert the Payment Type Category
+                this._context.usp_PaymentTypeCategoryUpsert(paymentTypeCategoryName);
+            }
+            catch (Exception ex)
+            {
+                // log the error and then re-throw it to ensure anywhere else that needs to handle the error can still do so
+                this._logger.LogError(ex.InnerException.Message);
+                throw;
+            }
         }
 
         public void DeleteExpense(int expenseID)

ab0b4e2785a18dbdfd6098df4a3dec6964b71621
durimdead <david.lancellotti@gmail.com>
2025-04-07 11:11:43 -0400

    updated the rest of the function header comments for the sprocs

--- api/FinanceApi/FinanceApi/Repositories/FinancialAppContext.cs
+++ api/FinanceApi/FinanceApi/Repositories/FinancialAppContext.cs
@@ -17,6 +17,13 @@ namespace FinanceApi.Repositories
         public virtual DbSet<vExpense> vExpense { get; set; }
         public virtual DbSet<vExpenseDetails> VExpenseDetails { get; set; }
 
+        /// <summary>
+        /// Update / insert record to the PeriodicElement table
+        /// </summary>
+        /// <param name="periodicElementName">Name of Periodic Element</param>
+        /// <param name="periodicElementSymbol">Symbol of element</param>
+        /// <param name="periodicElementWeight">Weight of element</param>
+        /// <param name="periodicElementID">ID of the record (only required for update - has default of "0" for inserts)</param>
         public void usp_PeriodicElementUpsert(string periodicElementName, string periodicElementSymbol, double periodicElementWeight, int periodicElementID = 0)
         {
             // parameterize the data for executing the stored procedure
@@ -30,6 +37,10 @@ namespace FinanceApi.Repositories
             this.Database.ExecuteSqlRaw("exec usp_PeriodicElementUpsert @periodicElementID, @periodicElementName, @periodicElementSymbol, @periodicElementWeight", parameters);
         }
 
+        /// <summary>
+        /// Delete a Periodic Element record from the database
+        /// </summary>
+        /// <param name="periodicElementID">ID of record to delete</param>
         public void usp_PeriodicElementDelete(int periodicElementID)
         {
             // parameterize the data for executing the stored procedure
@@ -40,6 +51,12 @@ namespace FinanceApi.Repositories
             this.Database.ExecuteSqlRaw("exec usp_PeriodicElementDelete @periodicElementID", parameters);
         }
 
+        /// <summary>
+        /// Update / insert record to the ExpenseType table
+        /// </summary>
+        /// <param name="expenseTypeName">name of expense type</param>
+        /// <param name="expenseTypeDescription">description of expense type</param>
+        /// <param name="expenseTypeID">ID of the record (only required for update - has default of "0" for inserts)</param>
         public void usp_ExpenseTypeUpsert(string expenseTypeName, string expenseTypeDescription, int expenseTypeID = 0)
         {
             // parameterize the data for executing the stored procedure
@@ -53,9 +70,9 @@ namespace FinanceApi.Repositories
         }
 
         /// <summary>
-        /// delete expense type record from database given expenseTypeID
+        /// Delete expense type record from database given expenseTypeID
         /// </summary>
-        /// <param name="expenseTypeID">expense type ID of record to delete</param>
+        /// <param name="expenseTypeID">ID of record to delete</param>
         public void usp_ExpenseTypeDelete(int expenseTypeID)
         {
             // parameterize the data for executing the stored procedure
@@ -66,6 +83,11 @@ namespace FinanceApi.Repositories
             this.Database.ExecuteSqlRaw("exec usp_ExpenseTypeDelete @expenseTypeID", parameters);
         }
 
+        /// <summary>
+        /// Update / insert record to the PaymentTypeCategory table
+        /// </summary>
+        /// <param name="paymentTypeCategoryName">Name of payment type category</param>
+        /// <param name="paymentTypeCategoryID">ID of the record (only required for update - has default of "0" for inserts)</param>
         public void usp_PaymentTypeCategoryUpsert(string paymentTypeCategoryName, int paymentTypeCategoryID = 0)
         {
             // parameterize the data for executing the stored procedure
@@ -77,6 +99,10 @@ namespace FinanceApi.Repositories
             this.Database.ExecuteSqlRaw("exec usp_PaymentTypeCategoryUpsert @paymentTypeCategoryID, @paymentTypeCategoryName", parameters);
         }
 
+        /// <summary>
+        /// Delete payment type category record from database
+        /// </summary>
+        /// <param name="paymentTypeCategoryID">ID of record to delete</param>
         public void usp_PaymentTypeCategoryDelete(int paymentTypeCategoryID)
         {
             // parameterize the data for executing the stored procedure
@@ -87,6 +113,13 @@ namespace FinanceApi.Repositories
             this.Database.ExecuteSqlRaw("exec usp_PaymentTypeCategoryDelete @paymentTypeCategoryID", parameters);
         }
 
+        /// <summary>
+        /// Update / insert record to the PaymentType table
+        /// </summary>
+        /// <param name="paymentTypeName">Name of the payment type</param>
+        /// <param name="paymentTypeDescription">description of the payment type</param>
+        /// <param name="paymentTypeCategoryID">ID of the category of the payment type</param>
+        /// <param name="paymentTypeID">ID of the record (only required for update - has default of "0" for inserts)</param>
         public void usp_PaymentTypeUpsert(string paymentTypeName, string paymentTypeDescription, int paymentTypeCategoryID, int paymentTypeID = 0)
         {
             // parameterize the data for executing the stored procedure
@@ -100,6 +133,10 @@ namespace FinanceApi.Repositories
             this.Database.ExecuteSqlRaw("exec usp_PaymentTypeUpsert @paymentTypeID, @paymentTypeName, @paymentTypeDescription, @paymentTypeCategoryID", parameters);
         }
 
+        /// <summary>
+        /// Delete payment type record from database
+        /// </summary>
+        /// <param name="paymentTypeID">ID of record to delete</param>
         public void usp_PaymentTypeDelete(int paymentTypeID)
         {
             // parameterize the data for executing the stored procedure
@@ -110,6 +147,17 @@ namespace FinanceApi.Repositories
             this.Database.ExecuteSqlRaw("exec usp_PaymentTypeDelete @paymentTypeID", parameters);
         }
 
+        /// <summary>
+        /// Update / insert record to the Expense table
+        /// </summary>
+        /// <param name="expenseTypeID">ID of the type of expense</param>
+        /// <param name="paymentTypeID">ID of the type of payment</param>
+        /// <param name="paymentTypeCategoryID">ID of the category of the payment type</param>
+        /// <param name="expenseDescription">Description of the expense</param>
+        /// <param name="isIncome">"true" if this is income (i.e. paycheck, selling of an item, etc)</param>
+        /// <param name="isInvestment">"true" if this money is being put into an investment vehicle (i.e. stocks, bonds, etc)</param>
+        /// <param name="expenseDate">Date the expense occurred</param>
+        /// <param name="expenseID">ID of the record (only required for update - has default of "0" for inserts)</param>
         public void usp_ExpenseUpsert(int expenseTypeID, int paymentTypeID, int paymentTypeCategoryID, string expenseDescription, bool isIncome, bool isInvestment, DateOnly expenseDate, int expenseID = 0)
         {
             // parameterize the data for executing the stored procedure
@@ -127,6 +175,10 @@ namespace FinanceApi.Repositories
             this.Database.ExecuteSqlRaw("exec usp_ExpenseUpsert @expenseID, @expenseTypeID, @paymentTypeID, @paymentTypeCategoryID, @expenseDescription, @isIncome, @isInvestment, @expenseDate", parameters);
         }
 
+        /// <summary>
+        /// Delete Expense record from database
+        /// </summary>
+        /// <param name="expenseID">ID of record to delete</param>
         public void usp_ExpenseDelete(int expenseID)
         {
             // parameterize the data for executing the stored procedure

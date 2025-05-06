b643594a386c8f402e77cb785d4d446e752e073a
durimdead <david.lancellotti@gmail.com>
2025-05-06 13:20:17 -0400

    updated environment variables in "development" to work for my local.

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+  apiUrl: 'https://localhost:7107/api/',
+  expensesController: 'Expenses/',
+  expenseTypesController: 'ExpenseTypes/',
+  paymentTypesController: 'PaymentTypes/',
+  search_expenseType_byName: 'SearchByExpenseTypeName',
+  search_paymentType_byName: 'SearchByPaymentTypeName',
+};

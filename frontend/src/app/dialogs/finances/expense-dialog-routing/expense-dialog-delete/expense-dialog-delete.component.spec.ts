8a8d46c33fcb8296ca32d9d36054d3e92032278f
durimdead <david.lancellotti@gmail.com>
2025-08-01 12:23:51 -0400

    fixed "required inputs" for unit tests

--- frontend/src/app/dialogs/finances/expense-dialog-routing/expense-dialog-delete/expense-dialog-delete.component.spec.ts
+++ frontend/src/app/dialogs/finances/expense-dialog-routing/expense-dialog-delete/expense-dialog-delete.component.spec.ts
@@ -1,18 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExpenseDialogDeleteComponent } from './expense-dialog-delete.component';
+import { Expense } from '../../../../../app.interfaces';
 
 describe('ExpenseDialogDeleteComponent', () => {
   let component: ExpenseDialogDeleteComponent;
   let fixture: ComponentFixture<ExpenseDialogDeleteComponent>;
 
+  const expenseData: Expense = {
+    expenseID: 0,
+    expenseTypeID: 0,
+    paymentTypeID: 0,
+    paymentTypeCategoryID: 0,
+    expenseTypeName: '',
+    paymentTypeName: '',
+    paymentTypeDescription: '',
+    paymentTypeCategoryName: '',
+    isIncome: false,
+    isInvestment: false,
+    expenseDescription: '',
+    expenseAmount: 0,
+    expenseDate: new Date(1, 1, 1),
+    lastUpdated: new Date(1, 1, 1),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ExpenseDialogDeleteComponent]
-    })
-    .compileComponents();
+      imports: [ExpenseDialogDeleteComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ExpenseDialogDeleteComponent);
+    fixture.componentRef.setInput('expenseData', expenseData);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- frontend/src/app/dialogs/finances/expense-dialog-routing/expense-dialog-edit/expense-dialog-edit.component.spec.ts
+++ frontend/src/app/dialogs/finances/expense-dialog-routing/expense-dialog-edit/expense-dialog-edit.component.spec.ts
@@ -7,10 +7,28 @@ import {
 
 import { ExpenseDialogEditComponent } from './expense-dialog-edit.component';
 import { HttpClient, HttpHandler } from '@angular/common/http';
+import { Expense } from '../../../../../app.interfaces';
 
 describe('ExpenseDialogEditComponent', () => {
   let component: ExpenseDialogEditComponent;
   let fixture: ComponentFixture<ExpenseDialogEditComponent>;
+
+  const expenseData: Expense = {
+    expenseID: 0,
+    expenseTypeID: 0,
+    paymentTypeID: 0,
+    paymentTypeCategoryID: 0,
+    expenseTypeName: '',
+    paymentTypeName: '',
+    paymentTypeDescription: '',
+    paymentTypeCategoryName: '',
+    isIncome: false,
+    isInvestment: false,
+    expenseDescription: '',
+    expenseAmount: 0,
+    expenseDate: new Date(1, 1, 1),
+    lastUpdated: new Date(1, 1, 1),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,11 +37,12 @@ describe('ExpenseDialogEditComponent', () => {
         { provide: MAT_DIALOG_DATA, useValue: {} },
         { provide: MatDialogRef, useValue: {} },
         HttpClient,
-		HttpHandler
+        HttpHandler,
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ExpenseDialogEditComponent);
+    fixture.componentRef.setInput('expenseData', expenseData);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

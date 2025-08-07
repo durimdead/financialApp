ffb0f0108be53c18396642487c4cdc3530d7c337
david.lancellotti <david.lancellotti@gmail.com>
2025-08-07 14:28:09 -0400

    - added new page not found component - updated route to allow for a "page does not exist" catch-all

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { FinancesComponent } from './finances/finances.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 
 export const AppRoutes: Routes = [
   // default route
@@ -12,9 +13,9 @@ export const AppRoutes: Routes = [
     path: 'about', // <your-domain>/about
     component: AboutComponent,
   },
-  // // "catch all" route (in the case that the route the user is attempting to navigate to doesn't exist)
-  // {
-  //     path: '**',
-  //     component: NotFoundComponent
-  // },
+  // "catch all" route (in the case that the route the user is attempting to navigate to doesn't exist)
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
 ];

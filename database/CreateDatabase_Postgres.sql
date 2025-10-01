ae2c1d134e05091ac50cf300f119ab6c4782c5c7
david.lancellotti <david.lancellotti@gmail.com>
2025-10-01 15:07:54 -0400

    created first postgres sproc

--- database/CreateDatabase_Postgres.sql
+++ database/CreateDatabase_Postgres.sql
@@ -93,3 +93,87 @@ ALTER TABLE public.expense
 *           
 *       #########################################################
 ************************************************************************/
+
+
+/************************************************************************
+*
+*
+*           BEGIN stored procedures
+*
+*
+************************************************************************/
+
+
+SET search_path TO FinancialApp;
+/*
+===========================================================================================================================================
+=    Author:
+=        David Lancellotti
+=
+=    Create date: 
+=        10/01/2025 03:00PM
+=
+=    Description:
+=        Update or insert an Expense Type with the relevant information
+=		 Returns "true" on success, "false" on failure with details in the "exception_" prepended variables
+=
+=    UPDATES:
+=                                DateTime
+=    Author                        mm/dd/yyyy HH:mm    Description
+=    =====================        =============        =======================================================================================
+=
+=
+===========================================================================================================================================
+*/
+CREATE OR REPLACE PROCEDURE public.expense_type_upsert(
+    expense_type_id_param INT
+    ,expense_type_name_param VARCHAR(50)
+	,expense_type_description_param VARCHAR(250)
+	,OUT was_success_out_param BOOLEAN
+	,OUT exception_message_text_out_param TEXT
+	,OUT exception_detail_out_param TEXT
+	,OUT exception_hint_out_param TEXT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	expense_type_id_param_value INT = expense_type_id_param;
+BEGIN
+	-- trim our varchar inputs to ensure we have no whitespace
+	SELECT TRIM(BOTH ' ' FROM expense_type_name_param) INTO expense_type_name_param;
+	SELECT TRIM(BOTH ' ' FROM expense_type_description_param) INTO expense_type_description_param;
+
+	-- if we can find a record with the ExpenseTypeID pushed in, let's update the information for it
+	IF EXISTS (SELECT FROM public.expense_type WHERE expense_type_id = expense_type_id_param) THEN
+		UPDATE public.expense_type
+		SET
+			expense_type_name = expense_type_name_param
+			,expense_type_description = expense_type_description_param
+		WHERE
+			expense_type_id = expense_type_id_param;
+	-- else, we check to see if the ExpenseTypeID sent in is 0 - indicating a new record
+	ELSIF (expense_type_id_param = 0) THEN
+		INSERT INTO public.expense_type(
+		expense_type_name
+		,expense_type_description
+		)
+		VALUES(
+			expense_type_name_param
+			,expense_type_description_param
+		);
+	-- if the ID doesn't exists and is not 0, the expense type doesn't exist and we can't update it.
+	ELSE
+		RAISE EXCEPTION 'The expense_type_id does not exist';
+	END IF;
+
+	was_success_out_param = true;
+
+	-- catch any exception that happens throughout the execution of the stored procedure
+	EXCEPTION
+		WHEN OTHERS THEN
+			GET STACKED DIAGNOSTICS exception_message_text_out_param = MESSAGE_TEXT,
+									exception_detail_out_param = PG_EXCEPTION_DETAIL,
+									exception_hint_out_param = PG_EXCEPTION_HINT;
+			was_success_out_param = false;
+END;
+$$;

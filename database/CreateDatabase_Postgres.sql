47bfceadb50b3aa7615cd3771b858087ee355d1f
david.lancellotti <david.lancellotti@gmail.com>
2025-10-02 12:21:07 -0400

    added in sproc for payment type category upsert

--- database/CreateDatabase_Postgres.sql
+++ database/CreateDatabase_Postgres.sql
@@ -137,13 +137,12 @@ CREATE OR REPLACE PROCEDURE public.expense_type_upsert(
 LANGUAGE plpgsql
 AS $$
 DECLARE
-	expense_type_id_param_value INT = expense_type_id_param;
 BEGIN
 	-- trim our varchar inputs to ensure we have no whitespace
 	SELECT TRIM(BOTH ' ' FROM expense_type_name_param) INTO expense_type_name_param;
 	SELECT TRIM(BOTH ' ' FROM expense_type_description_param) INTO expense_type_description_param;
 
-	-- if we can find a record with the ExpenseTypeID pushed in, let's update the information for it
+	-- if we can find a record with the expense_type_id pushed in, let's update the information for it
 	IF EXISTS (SELECT FROM public.expense_type WHERE expense_type_id = expense_type_id_param) THEN
 		UPDATE public.expense_type
 		SET
@@ -151,7 +150,7 @@ BEGIN
 			,expense_type_description = expense_type_description_param
 		WHERE
 			expense_type_id = expense_type_id_param;
-	-- else, we check to see if the ExpenseTypeID sent in is 0 - indicating a new record
+	-- else, we check to see if the expense_type_id sent in is 0 - indicating a new record
 	ELSIF (expense_type_id_param = 0) THEN
 		INSERT INTO public.expense_type(
 		expense_type_name
@@ -164,6 +163,73 @@ BEGIN
 	-- if the ID doesn't exists and is not 0, the expense type doesn't exist and we can't update it.
 	ELSE
 		RAISE EXCEPTION 'The expense_type_id does not exist';
+	END IF;
+
+	was_success_out_param = true;
+
+	-- catch any exception that happens throughout the execution of the stored procedure
+	EXCEPTION
+		WHEN OTHERS THEN
+			GET STACKED DIAGNOSTICS exception_message_text_out_param = MESSAGE_TEXT,
+									exception_detail_out_param = PG_EXCEPTION_DETAIL,
+									exception_hint_out_param = PG_EXCEPTION_HINT;
+			was_success_out_param = false;
+END;
+$$;
+
+
+/*
+===========================================================================================================================================
+=    Author:
+=        David Lancellotti
+=
+=    Create date: 
+=        10/02/2025 01:00PM
+=
+=    Description:
+=        Update or insert an Payment Type Category with the relevant information
+=
+=    UPDATES:
+=                                DateTime
+=    Author                        mm/dd/yyyy HH:mm    Description
+=    =====================        =============        =======================================================================================
+=
+=
+===========================================================================================================================================
+*/
+CREATE OR REPLACE PROCEDURE public.payment_type_category_upsert(
+    payment_type_category_id_param INT
+    ,payment_type_category_name_param VARCHAR(50)
+	,OUT was_success_out_param BOOLEAN
+	,OUT exception_message_text_out_param TEXT
+	,OUT exception_detail_out_param TEXT
+	,OUT exception_hint_out_param TEXT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+BEGIN
+	-- trim our varchar inputs to ensure we have no whitespace
+	SELECT TRIM(BOTH ' ' FROM payment_type_category_name_param) INTO payment_type_category_name_param;
+
+	-- if we can find a record with the payment_type_category_id pushed in, let's update the information for it
+	IF EXISTS (SELECT FROM public.payment_type_category WHERE payment_type_category_id = payment_type_category_id_param) THEN
+		UPDATE public.payment_type_category
+		SET
+			payment_type_category_name = payment_type_category_name_param
+		WHERE
+			payment_type_category_id = payment_type_category_id_param;
+	-- else, we check to see if the payment_type_category_id sent in is 0 - indicating a new record
+	ELSIF (payment_type_category_id_param = 0) THEN
+		INSERT INTO public.payment_type_category(
+			payment_type_category_name
+		)
+		VALUES(
+			payment_type_category_name_param
+		);
+	-- if the ID doesn't exists and is not 0, the payment type category doesn't exist and we can't update it.
+	ELSE
+		RAISE EXCEPTION 'The payment_type_category_id % does not exist', payment_type_category_id_param;
 	END IF;
 
 	was_success_out_param = true;
@@ -279,6 +345,8 @@ FROM
 *
 *
 *           BEGIN Aliased Views
+*					Used to avoid updating C# WebAPI models
+*					when utilizing postgres as database.
 *
 *
 ************************************************************************/
@@ -358,6 +426,23 @@ FROM
 *       #########################################################
 ************************************************************************/
 
+-- expense type
+INSERT INTO public.expense_type(expense_type_name, expense_type_description)
+VALUES('other', 'this expense does not fit into any other category')
+
+-- payment type category
+INSERT INTO public.payment_type_category(payment_type_category_name)
+VALUES('cash');
+
+-- payment type
+INSERT INTO public.payment_type(payment_type_name, payment_type_description, payment_type_category_id)
+VALUES	('cash', 'hard currency physically changing hands (i.e. not a cash app)', 1)
+		,('cash app', 'virtual currency changing hands (i.e. paypal, venmo, zelle, etc.)', 1);
+
+-- expense
+insert into public.expense(expense_type_id, payment_type_id, payment_type_category_id, expense_description, is_income, is_investment, expense_date, expense_amount)
+VALUES(1, 1, 1, 'sample hard cash transation', 0::bit, 0::bit, now(), 100.00)
+		,(1, 2, 1, 'sample cash app transation', 0::bit, 0::bit, now(), 234.22);
 
 /*
 select * from public.v_expense;

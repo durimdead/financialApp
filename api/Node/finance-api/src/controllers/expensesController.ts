fb4c80df7ca1a915c3bc6d06db05acb03788d95b
david.lancellotti <david.lancellotti@gmail.com>
2025-10-07 01:38:52 -0400

    extracted sample data into a service

--- api/Node/finance-api/src/controllers/expensesController.ts
+++ api/Node/finance-api/src/controllers/expensesController.ts
@@ -1,66 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
+import ExpenseRepoService from '../services/expenseRepoService';
 // import { items, Item } from '../models/item';
+
+const expenseRepoService: ExpenseRepoService = new ExpenseRepoService();
 
 // Read all items
 export const getExpenses = (req: Request, res: Response, next: NextFunction) => {
   try {
-    res.json({
-      httpStatusCode: 200,
-      expenseData: [
-        {
-          expenseID: 1,
-          expenseTypeName: "other",
-          paymentTypeName: "cash",
-          paymentTypeCategoryName: "cash",
-          isIncome: false,
-          isInvestment: false,
-          expenseTypeID: 1,
-          paymentTypeID: 1,
-          paymentTypeDescription:
-            "hard currency physically changing hands (i.e. not a cash app)",
-          paymentTypeCategoryID: 1,
-          expenseDate: "2025-10-03T00:00:00",
-          lastUpdated: "0001-01-01T00:00:00",
-          expenseDescription: "sample hard cash transation",
-          expenseAmount: 100,
-        },
-        {
-          expenseID: 2,
-          expenseTypeName: "other",
-          paymentTypeName: "cash app",
-          paymentTypeCategoryName: "cash",
-          isIncome: false,
-          isInvestment: false,
-          expenseTypeID: 1,
-          paymentTypeID: 2,
-          paymentTypeDescription:
-            "virtual currency changing hands (i.e. paypal, venmo, zelle, etc.)",
-          paymentTypeCategoryID: 1,
-          expenseDate: "2025-10-03T00:00:00",
-          lastUpdated: "0001-01-01T00:00:00",
-          expenseDescription: "sample cash app transation",
-          expenseAmount: 234.01,
-        },
-        {
-          expenseID: 4,
-          expenseTypeName: "other",
-          paymentTypeName: "cash app",
-          paymentTypeCategoryName: "cash",
-          isIncome: false,
-          isInvestment: false,
-          expenseTypeID: 1,
-          paymentTypeID: 2,
-          paymentTypeDescription:
-            "virtual currency changing hands (i.e. paypal, venmo, zelle, etc.)",
-          paymentTypeCategoryID: 1,
-          expenseDate: "2025-10-03T00:00:00",
-          lastUpdated: "0001-01-01T00:00:00",
-          expenseDescription: "another new test expense",
-          expenseAmount: 100.02,
-        },
-      ],
-      errorMessage: "",
-    });
+	let expenseData = expenseRepoService.getSampleExpenseData();
+	let returnValue = {
+    httpStatusCode: 200,
+    expenseData: expenseData,
+    errorMessage: "",
+  };
+    res.json(returnValue);
   } catch (error) {
     next(error);
   }
---
+++ api/Node/finance-api/src/services/expenseRepoService.ts
@@ -0,0 +1,58 @@
+export default class ExpenseRepoService {
+	getSampleExpenseData() {
+		let expenseDataArray = [
+			{
+				expenseID: 1,
+				expenseTypeName: "other",
+				paymentTypeName: "cash",
+				paymentTypeCategoryName: "cash",
+				isIncome: false,
+				isInvestment: false,
+				expenseTypeID: 1,
+				paymentTypeID: 1,
+				paymentTypeDescription:
+				"hard currency physically changing hands (i.e. not a cash app)",
+				paymentTypeCategoryID: 1,
+				expenseDate: "2025-10-03T00:00:00",
+				lastUpdated: "0001-01-01T00:00:00",
+				expenseDescription: "sample hard cash transation",
+				expenseAmount: 100,
+			},
+			{
+				expenseID: 2,
+				expenseTypeName: "other",
+				paymentTypeName: "cash app",
+				paymentTypeCategoryName: "cash",
+				isIncome: false,
+				isInvestment: false,
+				expenseTypeID: 1,
+				paymentTypeID: 2,
+				paymentTypeDescription:
+				"virtual currency changing hands (i.e. paypal, venmo, zelle, etc.)",
+				paymentTypeCategoryID: 1,
+				expenseDate: "2025-10-03T00:00:00",
+				lastUpdated: "0001-01-01T00:00:00",
+				expenseDescription: "sample cash app transation",
+				expenseAmount: 234.01,
+			},
+			{
+				expenseID: 4,
+				expenseTypeName: "other",
+				paymentTypeName: "cash app",
+				paymentTypeCategoryName: "cash",
+				isIncome: false,
+				isInvestment: false,
+				expenseTypeID: 1,
+				paymentTypeID: 2,
+				paymentTypeDescription:
+				"virtual currency changing hands (i.e. paypal, venmo, zelle, etc.)",
+				paymentTypeCategoryID: 1,
+				expenseDate: "2025-10-03T00:00:00",
+				lastUpdated: "0001-01-01T00:00:00",
+				expenseDescription: "another new test expense",
+				expenseAmount: 100.02,
+			},
+		];
+		return expenseDataArray;
+	}
+}

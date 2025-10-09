cd316a27277c89d8d751a8b79395c7fd7ca9c6df
david.lancellotti <david.lancellotti@gmail.com>
2025-10-08 21:21:32 -0400

    fixed the model mapping to ensure it properly aligned with the query columns

--- api/Node/finance-api/src/entity/v_expense_details.ts
+++ api/Node/finance-api/src/entity/v_expense_details.ts
@@ -1,4 +1,4 @@
-import { Entity, ViewEntity, ViewColumn, DataSource } from "typeorm";
+import { ViewEntity, ViewColumn} from "typeorm";
 
 @ViewEntity({
   expression: `
@@ -24,13 +24,13 @@ export class v_expense_details {
   @ViewColumn({name: 'expense_id'})
   ExpenseID?: number;
 
-  @ViewColumn({name: 'expense_date'})
+  @ViewColumn({name: 'expense_type_id'})
   ExpenseTypeID?: number;
 
-  @ViewColumn({name: 'expense_description'})
+  @ViewColumn({name: 'payment_type_id'})
   PaymentTypeID?: number;
 
-  @ViewColumn({name: 'expense_amount'})
+  @ViewColumn({name: 'payment_type_category_id'})
   PaymentTypeCategoryID?: number;
 
   @ViewColumn({name: 'expense_type_name'})
@@ -39,25 +39,25 @@ export class v_expense_details {
   @ViewColumn({name: 'payment_type_name'})
   PaymentTypeName?: string;
 
-  @ViewColumn({name: 'payment_type_category_name'})
+  @ViewColumn({name: 'payment_type_description'})
   PaymentTypeDescription?: string;
 
-  @ViewColumn({name: 'is_income'})
+  @ViewColumn({name: 'payment_type_category_name'})
   PaymentTypeCategoryName?: string;
 
-  @ViewColumn({name: 'is_investment'})
+  @ViewColumn({name: 'is_income'})
   IsIncome?: boolean;
 
-  @ViewColumn({name: 'expense_type_id'})
+  @ViewColumn({name: 'is_investment'})
   IsInvestment?: boolean;
 
-  @ViewColumn({name: 'payment_type_id'})
+  @ViewColumn({name: 'expense_description'})
   ExpenseDescription?: string;
 
-  @ViewColumn({name: 'payment_type_description'})
+  @ViewColumn({name: 'expense_amount'})
   ExpenseAmount?: number;
 
-  @ViewColumn({name: 'payment_type_category_id'})
+  @ViewColumn({name: 'expense_date'})
   ExpenseDate?: Date;
 
   // @ViewColumn()

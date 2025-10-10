48f30894a1d35b9d027a7bf37ca7c824b248adcd
david.lancellotti <david.lancellotti@gmail.com>
2025-10-10 14:55:36 -0400

    added in endpoints for getting vExpense data

--- api/Node/finance-api/src/services/expenseRepoService.ts
+++ api/Node/finance-api/src/services/expenseRepoService.ts
@@ -1,18 +1,28 @@
 import { AppDataSource } from "../data-source";
 // import { v_expense_detail } from "../entity/v_expense_detail_old";
 import { v_expense_detail } from "../entity/v_expense_details"
-import { v_expenses } from "../entity/v_expenses";
+import { v_expense } from "../entity/v_expense";
 
 export default class ExpenseRepoService {
-	private vExpenseDetail = AppDataSource.getRepository(v_expense_detail);
+  private vExpenseDetail = AppDataSource.getRepository(v_expense_detail);
+  private vExpense = AppDataSource.getRepository(v_expense);
 
-	public async getAllExpenseDetails() {
-		const expenses = await this.vExpenseDetail.find();
-		return expenses;
-	}
+  public async getAllExpenseDetails() {
+    const expenses = await this.vExpenseDetail.find();
+    return expenses;
+  }
 
-	public async getExpenseDetailsById(expenseId: number){
-		const expenses = await this.vExpenseDetail.findBy({ExpenseID: expenseId});
-		return expenses;
-	}
+  public async getExpenseDetailsById(expenseId: number) {
+    const expenses = await this.vExpenseDetail.findBy({ ExpenseID: expenseId });
+    return expenses;
+  }
+
+  public async getExpenses() {
+    const expenses = await this.vExpense.find();
+  }
+
+  public async getExpenseById(expenseId: number) {
+    const expenses = await this.vExpense.findBy({ ExpenseID: expenseId });
+    return expenses;
+  }
 }

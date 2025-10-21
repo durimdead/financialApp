a7485b062e1fcd486d3206dd56647c01ad2e4648
david.lancellotti <david.lancellotti@gmail.com>
2025-10-21 12:27:35 -0400

    added proper function header comments for expenses controller

--- api/Node/finance-api/src/controllers/expensesController.ts
+++ api/Node/finance-api/src/controllers/expensesController.ts
@@ -3,7 +3,12 @@ import ExpenseRepoService from '../services/expenseRepoService';
 
 const expenseRepoService: ExpenseRepoService = new ExpenseRepoService();
 
-// Read all items
+/**
+ * Gets all expenses in the database with their full details
+ * @param req the request object sent in
+ * @param res sends back {httpStatusCode:number, expenseData:[], errorMessage: string}
+ * @param next used for error handling
+ */
 export const getExpenses = async (req: Request, res: Response, next: NextFunction) => {
   try {
 	let expenseData = await expenseRepoService.getAllExpenseDetails();
@@ -18,12 +23,17 @@ export const getExpenses = async (req: Request, res: Response, next: NextFunctio
   }
 };
 
+/**
+ * To add a new Expense to the database. Information required is sent in through the request object as a json object in the body
+ * @param req req.body requires {expenseTypeID: number, paymentTypeID: number, paymentTypeCategoryID: number, expenseDescription: string, isIncome: boolean, isInvestment: boolean, expenseDate: Date, expenseAmount: decimal
+ * @param res sends back {httpStatusCode:number, errorMessage: string}
+ * @param next used for error handling
+ */
 export const postExpenses = async (req: Request, res: Response, next: NextFunction) => {
 	try{
 		const expenseDetails = req.body;
-		console.log('post expenses');
 		const result = await expenseRepoService.upsertExpense(
-			expenseDetails.expenseID,
+			0, // expenseID - 0 indicates it is a new expense being added
 			expenseDetails.expenseTypeID,
 			expenseDetails.paymentTypeID,
 			expenseDetails.paymentTypeCategoryID,
@@ -40,6 +50,12 @@ export const postExpenses = async (req: Request, res: Response, next: NextFuncti
 	}
 }
 
+/**
+ * To update an Expense in the database. Information required is sent in through the request object as a json object in the body
+ * @param req req.body requires {expenseID: number, expenseTypeID: number, paymentTypeID: number, paymentTypeCategoryID: number, expenseDescription: string, isIncome: boolean, isInvestment: boolean, expenseDate: Date, expenseAmount: decimal
+ * @param res sends back {httpStatusCode:number, errorMessage: string}
+ * @param next used for error handling
+ */
 export const putExpenses = async (req: Request, res: Response, next: NextFunction) => {
 	try{
 		const expenseDetails = req.body;

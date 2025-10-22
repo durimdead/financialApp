96e8a3449e738e778ddac0331e2155166233e480
david.lancellotti <david.lancellotti@gmail.com>
2025-10-22 12:35:08 -0400

    added api endpoint documentation comments

--- api/Node/finance-api/src/controllers/paymentTypesController.ts
+++ api/Node/finance-api/src/controllers/paymentTypesController.ts
@@ -3,6 +3,12 @@ import ExpenseRepoService from "../services/expenseRepoService";
 
 const expenseRepoService: ExpenseRepoService = new ExpenseRepoService();
 
+/**
+ * Gets all payment types in the database as an array
+ * @param req the request object sent in - no special input data required here
+ * @param res sends back {httpStatusCode: number, paymentTypeData: [], errorMessage: string}
+ * @param next used for error handling
+ */
 export const getPaymentTypes = async (req: Request, res: Response, next: NextFunction) => {
 	try{
 		const paymentTypeData = await expenseRepoService.getAllPaymentTypes();
@@ -17,6 +23,12 @@ export const getPaymentTypes = async (req: Request, res: Response, next: NextFun
 	}
 }
 
+/**
+ * Gets all payment types based on a search string (performs a partial search). This string is sent in as a url param ("" by default if not found, which will return all results)
+ * @param req the request object sent in - no special input data required here
+ * @param res sends back {httpStatusCode: number, paymentTypeData: [], errorMessage: string}
+ * @param next used for error handling
+ */
 export const getPaymentTypesBySearchString = async (req: Request, res: Response, next: NextFunction) => {
 	try{
 		const searchString = req.query.paymentTypeSearchString?.toString() ?? "";

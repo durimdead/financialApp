5294136e855cee23b4c4babbdc9780becfed9c99
david.lancellotti <david.lancellotti@gmail.com>
2025-10-24 11:11:36 -0400

    fixed dotenv config issue

--- api/Node/finance-api/src/data-source.ts
+++ api/Node/finance-api/src/data-source.ts
@@ -1,13 +1,15 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || "FinancialAppAccount",
-  password: process.env.DB_PASSWORD || "myTestAccount123",
-  database: process.env.DB_NAME || "FinancialApp",
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: false, // Use with caution in production; consider migrations
   logging: false,
   entities: ["src/entity/**/*.ts"], // Path to your entity files

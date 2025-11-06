69b3e668f55b0aa03ce78721a278947873f6f2de
david.lancellotti <david.lancellotti@gmail.com>
2025-11-06 12:50:29 -0500

    updated the front-end to utilize the C# api - also added in comments for which is which

--- frontend/angular/src/environments/environment.ts
+++ frontend/angular/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-//   apiUrl: 'https://localhost:7107/api/',
-  apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'https://localhost:7107/api/', // .NET Web API
+  //apiUrl: 'http://localhost:3000/api/', // Node with Express Web API
   expensesController: 'Expenses/',
   expenseTypesController: 'ExpenseTypes/',
   paymentTypesController: 'PaymentTypes/',
